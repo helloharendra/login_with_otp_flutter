9e3bfdd91fc519cd23233e26adce45b9e41abc39
Harendra Prajapati <78723011+helloharendra@users.noreply.github.com>
2023-08-20 21:30:36 +0530

    Created UI
    Designed UI to enter mobile number and otp

---
+++ lib/otp.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
+
+class MyOtp extends StatefulWidget {
+  const MyOtp({super.key});
+
+  @override
+  State<MyOtp> createState() => _MyOtpState();
+}
+
+class _MyOtpState extends State<MyOtp> {
+  TextEditingController otp = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: TextStyle(
+          fontSize: 20,
+          color: Color.fromRGBO(30, 60, 87, 1),
+          fontWeight: FontWeight.w600),
+      decoration: BoxDecoration(
+        border: Border.all(color: Color.fromRGBO(234, 239, 243, 1)),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+
+    final focusedPinTheme = defaultPinTheme.copyDecorationWith(
+      border: Border.all(color: Color.fromRGBO(114, 178, 238, 1)),
+      borderRadius: BorderRadius.circular(8),
+    );
+
+    final submittedPinTheme = defaultPinTheme.copyWith(
+      decoration: defaultPinTheme.decoration?.copyWith(
+        color: Color.fromRGBO(234, 239, 243, 1),
+      ),
+    );
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(left: 25, right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/user_login.png',
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Enter OTP',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'We need registerphone number before getting strted ',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Pinput(
+                length: 6,
+                // validator: (s) {
+                //   return s == '2222' ? null : 'Pin is incorrect';
+                // },
+                // pinputAutovalidateMode: PinputAutovalidateMode.onSubmit,
+                showCursor: true,
+                onCompleted: (pin) => print(pin),
+              ),
+              const SizedBox(
+                height: 45,
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      primary: const Color.fromARGB(255, 139, 14, 241),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  child: const Text('Verify phone number'),
+                ),
+              ),
+              Row(
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        'Edit Phone Number ?',
+                        style: TextStyle(color: Colors.black),
+                      ))
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/phone.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:login_with_otp_flutter/otp.dart';
+
+class MyPhone extends StatefulWidget {
+  const MyPhone({super.key});
+
+  @override
+  State<MyPhone> createState() => _MyPhoneState();
+}
+
+class _MyPhoneState extends State<MyPhone> {
+  TextEditingController countryCode = TextEditingController();
+  @override
+  void initState() {
+    // TODO: implement initState
+    countryCode.text = '+91';
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return 
+
+    Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(left: 25, right: 25),
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'images/user_login.png',
+                width: 150,
+                height: 150,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Text(
+                'Phone Verification',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'We need registerphone number before getting strted ',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.grey),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: SizedBox(
+                          width: 40,
+                          child: TextField(
+                            controller: countryCode,
+                            decoration:
+                                const InputDecoration(border: InputBorder.none),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                        child: Text(
+                          '|',
+                          style: TextStyle(fontSize: 30, color: Colors.grey),
+                        ),
+                      ),
+                      const Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(border: InputBorder.none),
+                        ),
+                      )
+                    ],
+                  )
+
+                  // const Row(
+                  // children: [
+                  // SizedBox(
+                  //   height: 45,
+                  //   child: TextField(),
+                  // ),
+                  // SizedBox(
+                  //   width: 10,
+                  // ),
+                  // SizedBox(
+                  //   height: 45,
+                  //   child: TextField(),
+                  // )
+                  // ],
+                  // ),
+                  ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => MyOtp()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                      primary: const Color.fromARGB(255, 139, 14, 241),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  child: const Text('Sent the code'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  
+  }
+}
